87a652589ea0d95ac7dd9536047476869625309a
scifiss <rebecca.shuang@gmail.com>
2017-09-21 16:54:19 -0500

    Add files via upload

--- model_gen.py
+++ model_gen.py
@@ -30,47 +30,35 @@ import sklearn
 import keras
 
 from random import shuffle
-# include 
-# 1. first track normal running
-# 2. first track reverse running         
-# 3. second track normal running        
 
 samples = []
-
+angles = []
 correctedAngle = 0.1
-# data collected by me on 1st track, reverse driving is included
+# data collected by me on 1st track
+# include 
+# 1. normal running in the center
+# 2. reverse running in the center
+# 3. recovery from left and right sides of the lane
+# 4. two more runs on the brick bridge
 
 with open('alldata/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
-    #next(reader,None)
+    #next(reader,None)   # use this if there is a header
     for line in reader:
         angle = float(line[3])
-        if (angle!=0):
-            samples.append(line)            
+        if (angle!=0):    # to remove the big block of data with zero angles
+            samples.append(line)         
+            angles.append(angle)
          
-#n1, bins, patches = plt.hist(angles, 50)  
-#plt.show()          
-#n=0            
-#with open('alldata/driving_log.csv') as csvfile:
-#    reader = csv.reader(csvfile)
-#    #next(reader,None)
-#    for line in reader:
-#        angle = float(line[3])
-#        if (angle==0):
-#            samples.append(line)
-#            angles.append(angle)
-#            n=n+1  
-#            if n>0.1*i:
-#                break
-#n1, bins, patches = plt.hist(angles, 50)  
-#plt.show()           
+n1, bins, patches = plt.hist(angles, 50)  
+plt.show()          
+
 sample = samples[0]        
 name = './alldata/IMG/'+sample[0].split('\\')[-1]
 
 image = cv2.cvtColor(cv2.imread(name), cv2.COLOR_BGR2RGB)
 height,width,channels = image.shape
 #plt.imshow(image)
-
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 Batch_size=128
@@ -84,15 +72,15 @@ def generator(samples, batch_size=Batch_size):
             images = []
             angles = []
             for batch_sample in batch_samples:
-                angle = float(batch_sample[3])
- #               if (angle!=0):
+                angle = float(batch_sample[3]) 
                 for i in range(3):
-                    name = './alldata/IMG/'+batch_sample[i].split('\\')[-1]
-                    print(name)                     
+                    name = './alldata/IMG/'+batch_sample[i].split('\\')[-1]                                 
                     image = cv2.cvtColor(cv2.imread(name), cv2.COLOR_BGR2RGB)                       
                     images.append(image)  # orig image
+                    # add Gaussian noises to original images
                     gaussian_noise =random_noise(image,mode='gaussian',var=0.001)*255
                     images.append(gaussian_noise.astype('uint8') ) # noised image                  
+                    # flip images vertically
                     images.append(cv2.flip(image,1)) # flipped image
                                    
                 angles.append(angle)  # orig center
@@ -104,33 +92,25 @@ def generator(samples, batch_size=Batch_size):
                 angles.append(angle-correctedAngle)     # orig right        
                 angles.append(angle-correctedAngle)     # noised right    
                 angles.append((angle-correctedAngle) * -1.0) # flipped right               
-
-            # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)        
 
 # compile and train the model using the generator function
-
 train_generator = generator(train_samples, batch_size=Batch_size)
 validation_generator = generator(validation_samples, batch_size=Batch_size)
-
-
-
-# to show an image
-
-#plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))      
 
 model = Sequential()
 model.add(Lambda(lambda x: x/255.0-0.5,
         input_shape=( height, width,3)))
-model.add(Cropping2D(cropping=((75,25),(0,0))))
+# trim image to only see section with road
+model.add(Cropping2D(cropping=((80,25),(0,0))))  # (None, 60, 320, 3)
 #print(model.layers[-1].output_shape)
 model.add(Convolution2D(6, 5, 5, activation="relu"))  # (None, 56, 316, 6)
 model.add(MaxPooling2D())  # (None, 28, 158, 6)
 model.add(Convolution2D(16, 5, 5, activation="relu")) #(None, 24, 154, 16)
 model.add(MaxPooling2D()) # (None, 12, 77, 16)
-#model.add(Dropout(0.5)) # (None, 12, 77, 16)
+model.add(Dropout(0.7)) # (None, 12, 77, 16)
 model.add(Flatten()) # (None, 14784)
 #model.add(Dense(560))
 #model.add(Dropout(0.7))
@@ -139,18 +119,12 @@ model.add(Dropout(0.7))
 model.add(Dense(84))
 model.add(Dense(10))
 model.add(Dense(1))
-opt = keras.optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+opt = keras.optimizers.Adam(lr=0.0003, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 model.compile(loss='mse', optimizer=opt)
 #model.compile(loss='mse', optimizer='adam')
 model.fit_generator(train_generator, samples_per_epoch= len(train_samples)*9, 
-                    validation_data=validation_generator, nb_val_samples=len(validation_samples)*9, nb_epoch=4)
+                    validation_data=validation_generator, nb_val_samples=len(validation_samples)*9, nb_epoch=10)
 
 model.save('model.h5')
-# data augmentation
-
-  
 
 gc.collect()
-
-
-# avoid overfitting/underfitting
